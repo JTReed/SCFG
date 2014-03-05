68627f59bdbbccb8cd1092be14aaf3eb30107a12
JTReed <reed.jacksont@gmail.com>
2014-03-04 23:03:58 -0600

    Updated player animations and added shot charge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,14 @@ using System.Collections;
 [RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
-    public float acceleration;
-    public float gravity;
-    public float jumpSpeed;
-    public float minJumpHeight;
-    public float maxJumpHeight;
-    public float slideDuration;
+    public float speed = 10;
+    public float acceleration = 100;
+    public float gravity = 80;
+    public float jumpSpeed = 5;
+    public float minJumpHeight = 5;
+    public float maxJumpHeight = 30;
+    public float slideDuration = 0.5f;
+    public float chargeThreshold = 3.0f;
     public Transform[] projectiles;
 
     private float m_currentSpeed;
@@ -20,9 +21,11 @@ public class PlayerController : MonoBehaviour {
     private Vector2 m_amountToMove;
 
     private float m_slideStartTime;
+    private float m_chargeStartTime;
 
     private bool m_jumping;
     private bool m_sliding;
+    private bool m_charging;
 
     private EntityPhysics m_physics;
     private Animator m_animator;
@@ -49,7 +52,7 @@ public class PlayerController : MonoBehaviour {
                 m_jumping = false;
             }
 
-            if (Input.GetAxisRaw("Vertical") < 0 && Input.GetButtonDown("Jump")) {
+            if (Input.GetAxisRaw("Vertical") < 0 && Input.GetButtonDown("Jump") && !m_sliding) {
                 m_sliding = true;
                 m_animator.SetBool("Sliding", true);
                 m_slideStartTime = Time.time;
@@ -92,8 +95,40 @@ public class PlayerController : MonoBehaviour {
         m_amountToMove.y -= gravity * Time.deltaTime;
         m_physics.Move(m_amountToMove * Time.deltaTime);
 
-        if (Input.GetButtonDown("Fire") && GameObject.FindGameObjectsWithTag("PlayerProjectile").Length < 3) {
+        if (Input.GetButtonDown("Fire") && GameObject.FindGameObjectsWithTag("PlayerProjectile").Length < 3 && !m_sliding) {
+            // can fire anytime except when sliding
+            m_charging = true;
             Instantiate(projectiles[0], transform.position, transform.rotation);
+            m_chargeStartTime = Time.time;
+        }
+
+        if (m_charging) {
+            float chargeTime = Time.time - m_chargeStartTime;
+            Debug.Log("chargetime: " + chargeTime + " , chargethresh: " + chargeThreshold);
+            if(Input.GetButton("Fire")) {
+                if (chargeTime >= chargeThreshold) {
+                    Debug.Log("FULLY CHARGED");
+                    m_animator.SetInteger("ChargeLevel", 2);
+                }
+                else if (chargeTime >= (chargeThreshold / 3.0f)) {
+                    Debug.Log("HALF CHARGED");
+                    m_animator.SetInteger("ChargeLevel", 1);
+                }
+            } else {
+                m_charging = false;
+                //Debug.Log("release charge");
+
+                if (chargeTime >= chargeThreshold) {
+                    //Debug.Log("PEW");
+                    Instantiate(projectiles[1], transform.position, transform.rotation);
+                    m_animator.SetInteger("ChargeLevel", 0);
+                }
+                else if (chargeTime >= chargeThreshold / 3.0f) {
+                    //Debug.Log("pew");
+                    Instantiate(projectiles[0], transform.position, transform.rotation);
+                    m_animator.SetInteger("ChargeLevel", 0);
+                }
+            }
         }
 	}
 
